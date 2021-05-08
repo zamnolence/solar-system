9815785a8fa1fa0e5c80aeb321918a9bc35cecec
Doungporn <dwiwat97@gmail.com>
2021-05-08 21:12:11 +0800

    Chapter 6 Add user profile page

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,11 @@ from app.forms      import LoginForm, RegistrationForm
 from app.models     import User
 from werkzeug.urls  import url_parse
 
+
+# Index
 @app.route('/')
 @app.route('/index')
-@login_required
+## @login_required
 def index():
   user = {'username': 'Squeebs'}
   posts = [
@@ -22,38 +24,52 @@ def index():
     ]
   return render_template('index.html', title='Home', posts=posts)
 
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:           # redirect if already logged in
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():               # validate login form
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
+        next_page = request.args.get('next')    # save requested (protected) URL
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(url_for('index'))
+            next_page = url_for('index')        # redirect to index if requested URL doesn't exist
+        return redirect(next_page)   # reditect to requested URL if exists
     return render_template('login.html', title='Sign In', form=form)
 
+# Logout
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+# Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:           # redirect if already logged in
         return redirect(url_for('index'))
     form = RegistrationForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():               # validate registration form
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
+        db.session.add(user)                    # add to db
+        db.session.commit()                     # commit to db
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+# User profile
+@app.route('/user/<username>')  # dynamic component username
+@login_required                 # only logged in user allow
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404() # first() if exists or sends 404 error if not exists
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)   
