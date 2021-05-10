c374fff63af523f18fa74ecb8de57a7544beda34
22601257 <22601257@student.uwa.edu.au>
2021-05-10 02:31:30 -0700

    Updated DB Models to account for Quiz attempts and their answers.

--- app/models.py
+++ app/models.py
@@ -5,37 +5,49 @@ from flask_login        import UserMixin
 from hashlib            import md5
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+  posts = db.relationship('Post', backref='author', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-    
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+  
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
-    def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
-            digest, size)
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+      digest, size)
 
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+  return User.query.get(int(id))
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  body = db.Column(db.String(140))
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  page = db.Column(db.String)
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
+  def __repr__(self):
+    return '<Post {}>'.format(self.body)
+
+class Quiz_Attempt(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  answers = db.relationship('Answer', backref='quiz_attempt', lazy='dynamic')
+  timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+class Answer(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  attempt_id = db.Column(db.Integer, db.ForeignKey('quiz_attempt.id'))
+  correct = db.Column(db.Integer)
