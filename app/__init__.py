1764afcc21e47a321b0db978dea4fcfec687025c
22601257 <22601257@student.uwa.edu.au>
2021-05-11 23:58:34 -0700

    Reformatted whitespace.

--- app/__init__.py
+++ app/__init__.py
@@ -8,42 +8,41 @@ from flask_migrate      import Migrate
 from flask_login        import LoginManager
 from flask_mail         import Mail
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+migrate = Migrate(app, db)
 
 # Log errors by email if we are not in debug mode
 if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Solar System Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+  if app.config['MAIL_SERVER']:
+    auth = None
+    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+      auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+      secure = ()
+    mail_handler = SMTPHandler(
+      mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+      fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+      toaddrs=app.config['ADMINS'], subject='Solar System Failure',
+      credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
-    if not os.path.exists('logs'): # Logging to file
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/solar-system.log', maxBytes=10240,
-                                       backupCount=10)  # keep the last 10 log files
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+  if not os.path.exists('logs'): # Logging to file
+    os.mkdir('logs')
+  file_handler = RotatingFileHandler('logs/solar-system.log', maxBytes=10240,
+                                      backupCount=10)  # keep the last 10 log files
+  file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)       # all messages are passed to handler
-    app.logger.info('Solar System startup')
+  app.logger.setLevel(logging.INFO)       # all messages are passed to handler
+  app.logger.info('Solar System startup')
 
 from app import routes, models, errors
--- app/routes.py
+++ app/routes.py
@@ -11,14 +11,14 @@ from dateutil       import tz
 # Record time of last visit
 @app.before_request
 def before_request():
-    from_zone = tz.tzutc()
-    to_zone = tz.tzlocal()
-    utc = datetime.utcnow()
-    utc = utc.replace(tzinfo=from_zone)
-    central = utc.astimezone(to_zone)   # convert to local timezone
-    if current_user.is_authenticated:
-        current_user.last_seen = central
-        db.session.commit()
+  from_zone = tz.tzutc()
+  to_zone = tz.tzlocal()
+  utc = datetime.utcnow()
+  utc = utc.replace(tzinfo=from_zone)
+  central = utc.astimezone(to_zone)   # convert to local timezone
+  if current_user.is_authenticated:
+    current_user.last_seen = central
+    db.session.commit()
 
 # Index view
 @app.route('/', methods=['GET', 'POST'])
@@ -53,93 +53,93 @@ def index():
 # Login view
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:           # redirect if already logged in
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():               # validate login form
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')    # save requested (protected) URL
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')        # redirect to index if requested URL doesn't exist
-        return redirect(next_page)   # reditect to requested URL if exists
-    return render_template('login.html', title='Sign In', form=form)
+  if current_user.is_authenticated:           # redirect if already logged in
+    return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():               # validate login form
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    next_page = request.args.get('next')    # save requested (protected) URL
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')        # redirect to index if requested URL doesn't exist
+    return redirect(next_page)   # reditect to requested URL if exists
+  return render_template('login.html', title='Sign In', form=form)
 
 # Logout view
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+  logout_user()
+  return redirect(url_for('index'))
 
 # Register view
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:           # redirect if already logged in
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():               # validate registration form
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)                    # add to db
-        db.session.commit()                     # commit to db
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+  if current_user.is_authenticated:           # redirect if already logged in
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():               # validate registration form
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)                    # add to db
+    db.session.commit()                     # commit to db
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Register', form=form)
 
 # User profile view
 @app.route('/user/<username>')  # dynamic component username
 ## @login_required                 # only logged in user allow
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404() # first() if exists or sends 404 error if not exists
-    return render_template('user.html', user=user, posts=user.posts)
+  user = User.query.filter_by(username=username).first_or_404() # first() if exists or sends 404 error if not exists
+  return render_template('user.html', user=user, posts=user.posts)
 
 # Edit profile view
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm(current_user.username)
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.about_me = form.about_me.data
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile',
-                           form=form)
+  form = EditProfileForm(current_user.username)
+  if form.validate_on_submit():
+    current_user.username = form.username.data
+    current_user.about_me = form.about_me.data
+    db.session.commit()
+    flash('Your changes have been saved.')
+    return redirect(url_for('edit_profile'))
+  elif request.method == 'GET':
+    form.username.data = current_user.username
+    form.about_me.data = current_user.about_me
+  return render_template('edit_profile.html', title='Edit Profile',
+                          form=form)
 
 # Reset password request view
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = ResetPasswordRequestForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_password_reset_email(user)
-        flash('Check your email for the instructions to reset your password')
-        return redirect(url_for('login'))
-    return render_template('reset_password_request.html',
-                           title='Reset Password', form=form)
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = ResetPasswordRequestForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user:
+      send_password_reset_email(user)
+    flash('Check your email for the instructions to reset your password')
+    return redirect(url_for('login'))
+  return render_template('reset_password_request.html',
+                          title='Reset Password', form=form)
 
 # Password reset view
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash('Your password has been reset.')
-        return redirect(url_for('login'))
-    return render_template('reset_password.html', form=form)
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  user = User.verify_reset_password_token(token)
+  if not user:
+    return redirect(url_for('index'))
+  form = ResetPasswordForm()
+  if form.validate_on_submit():
+    user.set_password(form.password.data)
+    db.session.commit()
+    flash('Your password has been reset.')
+    return redirect(url_for('login'))
+  return render_template('reset_password.html', form=form)
