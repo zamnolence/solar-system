1c8a6835c0653dbeca5d661348cf220a77b64d85
22601257 <22601257@student.uwa.edu.au>
2021-05-17 02:23:49 -0700

    Removed excess comments from database model.

--- app/models.py
+++ app/models.py
@@ -17,7 +17,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    # quiz_attempts = db.relationship('Attempt', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     score_child = db.relationship("Score", backref = 'user_parent')
@@ -162,47 +161,3 @@ class Score(db.Model):
       if  int(score) < 0:
         raise AssertionError('Score can be no less than 0. Provided: {}'.format(score))
       return score
-
-# class Attempt(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#   answers = db.relationship('Answer', backref='attempt', lazy='dynamic')
-#   timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-#   def __repr__(self):
-#     return 'Attempt ID: {}, User: {}, Progress: {}/10, Timestamp: {}'.format(
-#       self.id, self.user_id, self.answers.count(), self.timestamp)
-
-#   def add_answer(self, answer):
-#     att_answers = self.answers.all()    
-#     if len(att_answers) >= 10:
-#       return 1
-#     for a in att_answers:
-#       if answer.question == a.question:
-#         return 1
-#     db.session.add(answer)
-#     db.session.commit()
-#     return 0
-
-# class Answer(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   attempt_id = db.Column(db.Integer, db.ForeignKey('attempt.id'))
-#   question = db.Column(db.Integer)
-#   correct = db.Column(db.Integer)
-
-#   def __repr__(self):
-#     correct = 'Incorrect' if self.correct == 0 else 'Correct' 
-#     return 'Answer ID: {}, Attempt ID: {}, Question: {}, {}'.format(
-#       self.id, self.attempt_id, self.question, correct)
-  
-#   @validates('question')
-#   def validate_question(self, key, question):
-#     if question <= 0 or question > 10:
-#       raise AssertionError('Question must be between 1 and 10. Provided: {}'.format(question))
-#     return question
-
-#   @validates('correct')
-#   def validate_correct(self, key, correct):
-#     if correct not in [0, 1]:
-#       raise AssertionError('Correct must be 0 (correct) or 1 (incorrect). Provided: {}'.format(correct))
-#     return correct
